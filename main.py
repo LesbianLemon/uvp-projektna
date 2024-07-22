03fa631396d4773ea39258fa45e4b48500a79c3f
Andrej Anžlovar <andrej.anzlovar2@gmail.com>
2024-07-22 17:47:16 +0200

    Added BeautifulSoup functionality to webscraping.py

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 import typing
+import re
 
 from utils.webscraping import WebPage
 
 
 def main():
     # Website URL
-	url: str = "https://www.lpi.usra.edu/meteor/metbull.php?sea=%2A&sfor=names&valids=yes&stype=contains&lrec=50000&page=1"
+	url: str = "https://www.lpi.usra.edu/meteor/metbull.php?sea=%2A&sfor=names&valids=yes&stype=contains&lrec=50&map=ll&page=1"
 
 	page: WebPage = WebPage(url)
-	page.save_html("data/page1.html", force=True) # force=True to rewrite the file if it already exists
-
-
+	page.save_html("data/page1.html", force=False) # force=False do not rewrite the file if it already exists
+	page.init_parser()
 
 
 if __name__ == "__main__":
--- utils/webscraping.py
+++ utils/webscraping.py
@@ -6,7 +6,8 @@ import os.path
 import typing
 from typing import Optional
 
-class WebPage():
+
+class WebPage(bs.BeautifulSoup):
 	url: str
 	html_path: Optional[str]
 
@@ -21,7 +22,6 @@ class WebPage():
 
 		html : str, optional
 			| path to webpage HTML file.
-
 		"""
 
 		self.url = url
@@ -36,7 +36,6 @@ class WebPage():
 		-------
 		str
 			| HTML source code.
-
 		"""
 
 		# trick website into thinking its a request from a real browser
@@ -55,7 +54,7 @@ class WebPage():
 
 	def save_html(self, path: str, force: bool=False) -> bool:
 		"""
-		Saves HTML of given website URL to given path.
+		Saves HTML of given website URL to given path and adds said path to `html` class variable.
 
 		Parameters
 		----------
@@ -68,7 +67,6 @@ class WebPage():
 		-------
 		bool
 			| True if the write was successful and False if not.
-
 		"""
 
 		mode: str = "x" if not force else "w"
@@ -76,7 +74,32 @@ class WebPage():
 		try:
 			with open(path, mode) as file:
 				file.write(self.get_html())
+				print(f"File saved to `{path}` and `html_path` will be set to its path")
+				self.html_path = path
 				return True
+				
+		except Exception as e:
+			msg: str = "File could not be written and `html_path` was not set"
 
-		except FileExistsError:
+			if type(e) is FileExistsError:
+				msg = f"File `{path}` already exists, `html_path` will be set to that file path"
+				self.html_path = path
+			
+			print(msg)
 			return False
+
+	
+	def init_parser(self, parser: str="html.parser") -> None:
+		"""
+		Starts the parser if html_path variable is set.
+
+		Parameters
+		----------
+		parser : str, default="html.parser"
+			| a valid BeautifulSoup parser for the saved HTML file
+		"""
+
+		assert self.html_path, "html_path class variable is not set, make sure to run `class.save_html(path)`"
+
+		with open(self.html_path, "r") as html_doc:
+			super().__init__(html_doc.read(), parser)
