a3e5d3d0e082a6f70c797d9251ee211358db2880
Andrej Anžlovar <andrej.anzlovar2@gmail.com>
2024-07-23 11:34:11 +0200

    Page count scraping and downloading all the pages

--- main.py
+++ main.py
@@ -1,22 +1,72 @@
-import typing
 import re
 
-from utils.webscraping import WebPage
+from utils.webscraping import WebPage # locally sourced module
 
 
-def main():
-    # Website URL
-	url: str = "https://www.lpi.usra.edu/meteor/metbull.php?sea=%2A&sfor=names&valids=yes&stype=contains&lrec=50&map=ll&page=1"
+# save time and space by making a dedicated function for save feedback
+def print_save_success(return_type: int, page_num: int) -> None:
+	match return_type:
+		case 0:
+			print(f"Page {page_num} failed to be saved to `data/page{page_num}.html`.")
+		case 1:
+			print(f"Page {page_num} already has `data/page{page_num}.html`, using that instead.")
+		case 2:
+			print(f"Page {page_num} saved successfully to `data/page{page_num}.html`.") 
 
-	page: WebPage = WebPage(url)
-	page.save_html("data/page1.html", force=False) # force=False do not rewrite the file if it already exists
+
+# get a list of NON-INITIALISED WebPage elements (this is to reduce memory usage and processing time)
+def get_saved_pages(url: str, start_page:int, end_page: int, force: bool=False) -> list[WebPage]:
+	pages: list[WebPages] = []
+
+	page_num: int
+	for page_num in range(start_page, end_page + 1):
+		page: WebPage = WebPage(url + str(page_num))
+		return_type: int = page.save_html(f"data/page{page_num}.html", force=force)
+
+		print_save_success(return_type, page_num)
+		
+		pages.append(page)
+
+	return pages
+
+
+def main() -> None:
+	# website URL with preset search options:
+	#
+	# sea - search string (%2A = *)
+	# sfor - search for (names, text, places, classes, years)
+	# valids - only approved meteorites
+	# stype - type of search (contains, startswith, exact, soundslike)
+	# lrec - lines per page (20, 50, 100, 200, 500, 1000, 2000, 5000, 50000)
+	# map - display decimal degrees location
+	# page - page number inof search
+	url: str = "https://www.lpi.usra.edu/meteor/metbull.php?sea=%2A&sfor=names&valids=yes&stype=contains&lrec=5000&map=ll&page="
+	
+	# add "1", representing the first page, which should always exist
+	page: WebPage = WebPage(url + "1")
+	# save page 1 first, the rest come later
+	return_type: int = page.save_html("data/page1.html")
+
+	print_save_success(return_type, 1)
+
+	# we need page 1 to find how many pages there are
+	if return_type == 0:
+		print("Aborting, page 1 needs to be saved properly!")
+		return
+	
 	page.init_parser()
 
-	table = page.find("table", { "id": "maintable" })
+	# we do not initialise BeautifulSoup for a simple page count search (save memory and processing time)
+	# template <h4> text containing page number count: "Showing data for page 1 of 1514: records 1 - 50"
+	page_count: int = int(re.search(r"page \d+ of (\d+)", page.h4.text).group(1)) # match the second number and add it to group #1 then save result of group #1 match
 
-	page_count = table.caption
-	print(page_count)
-	# re.match(r".*?\d+.*?(\d+)") 
+	pages: list[WebPage] = []
+
+	if page_count > 1:
+		print("Site has multiple pages, continuing to download.")
+		pages = [page] + get_saved_pages(url, start_page=2, end_page=page_count, force=False) # start at page 2, since we have already save page 1
+	
+	print(*pages, sep="\n\n")
 
 
 if __name__ == "__main__":
--- utils/webscraping.py
+++ utils/webscraping.py
@@ -3,15 +3,14 @@ import bs4 as bs
 
 import os.path
 
-import typing
-from typing import Optional
-
 
 class WebPage(bs.BeautifulSoup):
+	# variable typing
 	url: str
-	html_path: Optional[str]
+	html_path: str | None
+	initialised: bool
 
-	def __init__(self, url: str, html_path: Optional[str]=None) -> None:
+	def __init__(self, url: str, html_path: str | None=None) -> None:
 		"""
 		WebPage class constructor.
 
@@ -26,8 +25,14 @@ class WebPage(bs.BeautifulSoup):
 
 		self.url = url
 		self.html_path = html_path
+		self.initialised = False
 
 
+	# useful for printing
+	def __str__(self):
+		return f"WebPage(url={self.url}, html_path={self.html_path}, initialised={self.initialised})"
+
+	
 	def get_html(self) -> str:
 		"""
 		Access the website URL and return the HTML source code representing the webpage.
@@ -39,7 +44,7 @@ class WebPage(bs.BeautifulSoup):
 		"""
 
 		# trick website into thinking its a request from a real browser
-		headers: dict = {
+		headers: dict[str, str] = {
 			"Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
 			"Accept-Encoding": "gzip, deflate",
 			"Accept-Language": "en-GB,en-US;q=0.9,en;q=0.8",
@@ -48,45 +53,43 @@ class WebPage(bs.BeautifulSoup):
 			"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36",
 		}
 		
+		# adding headers requires Session
 		session = req.Session()
 		return req.get(self.url, headers=headers).text
 
 
 	def save_html(self, path: str, force: bool=False) -> bool:
 		"""
-		Saves HTML of given website URL to given path and adds said path to `html` class variable.
+		Saves HTML of given website URL to given path and adds said path to `html_path` class variable.
 
 		Parameters
 		----------
 		path : str
 			| the location where to store the read HTML data.
-		force : bool, default=False
-			| whether to force over-writing the provided file path.
+		force : bool, default=`False`
+			| whether to force over-writing the provided file path (if `False` and path already exists it will also set `html_path` variable to said path)
 
 		Returns
 		-------
-		bool
-			| True if the write was successful and False if not.
+		int
+			| `0` if writing failed, `1` if the file is already present and force=False, `2` if writing was successful
 		"""
 
 		mode: str = "x" if not force else "w"
 
+		# if mode is "x" and file already exists a FileExistsError is thrown
 		try:
 			with open(path, mode) as file:
 				file.write(self.get_html())
-				print(f"File saved to `{path}` and `html_path` will be set to its path")
 				self.html_path = path
-				return True
+				return 2
 				
 		except Exception as e:
-			msg: str = "File could not be written and `html_path` was not set"
-
 			if type(e) is FileExistsError:
-				msg = f"File `{path}` already exists, `html_path` will be set to that file path"
 				self.html_path = path
+				return 1
 			
-			print(msg)
-			return False
+			return 0
 
 	
 	def init_parser(self, parser: str="html.parser") -> None:
@@ -95,11 +98,13 @@ class WebPage(bs.BeautifulSoup):
 
 		Parameters
 		----------
-		parser : str, default="html.parser"
-			| a valid BeautifulSoup parser for the saved HTML file
+		parser : str, default=`"html.parser"`
+			| a valid `BeautifulSoup` parser for the saved HTML file
 		"""
 
-		assert self.html_path, "html_path class variable is not set, make sure to run `class.save_html(path)`"
+		# make sure htaml_path is set before trying to initialise BeautifulSoup
+		assert self.html_path, "`html_path` class variable is not set, make sure to run `class.save_html(path)`"
 
 		with open(self.html_path, "r") as html_doc:
 			super().__init__(html_doc.read(), parser)
+			self.initialised = True
